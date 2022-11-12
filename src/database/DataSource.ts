f72f51f1f95675a4ec142e100f46cd9ea57d9e33
Diogo Alves de Andrade <diogo10-1@hotmail.com>
2022-11-11 21:51:30 -0300

    fixed port connection

--- src/database/DataSource.ts
+++ src/database/DataSource.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 export const dataSource = new DataSource({
     type: 'postgres',
     host: 'localhost',
-    port: 5433,
+    port: 5432,
     username: 'admin',
     password: 'ak&v4xbd',
     database: 'postgres',
